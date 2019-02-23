63bf11774c68d88428a74352260be20cfacde56f
/ /\ / /\/ <imaitland@gmail.com>
2019-02-22 22:00:27 -0500

    scrape down

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,18 +10,10 @@ export default class IndexPage extends React.Component {
 
     return (
       <Layout>
-        <section className="section">
           <div className="container">
-            <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-            </div>
             {posts
               .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #333', padding: '2em 4em' }}
-                  key={post.id}
-                >
+
                   <p>
                     <Link className="has-text-primary" to={post.fields.slug}>
                       {post.frontmatter.title}
@@ -29,18 +21,8 @@ export default class IndexPage extends React.Component {
                     <span> &bull; </span>
                     <small>{post.frontmatter.date}</small>
                   </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
-                  </p>
-                </div>
               ))}
           </div>
-        </section>
       </Layout>
     )
   }
